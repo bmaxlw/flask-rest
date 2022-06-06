2883f2143bc71ea8e5304469f00e016c85103128
Maksym Bondaruk <b.max.lw@gmail.com>
2022-06-06 12:01:34 +0300

    GET / POST / DELETE requests added

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from http import client
+from importlib.metadata import requires
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-clients = {
-    10000: {'full_name': 'James Robertson', 'phone_number': '+420-555-88-99', 'email_address': 'jrobertson@ico.cz'} 
-}
+clients = {}
+
+client_args = reqparse.RequestParser()
+client_args.add_argument("full_name", type=str, help='Full name undefined', required=True)
+client_args.add_argument("phone_number", type=str, help='Phone number undefined', required=True)
+client_args.add_argument("email_address", type=str, help='Email address undefined', required=True)
+
+def abort_if_id_not_exists(client_id):
+    if client_id not in clients:
+        abort(404, message='Client does not exist...')
+
+def abort_if_id_exists(client_id):
+    if client_id in clients:
+        abort(409, message='Client already exists...')
 
 class Client(Resource):
     def get(self, client_id):
-        return clients[client_id]
+        abort_if_id_not_exists(client_id)
+        return {client_id: clients[client_id]}
 
-api.add_resource(Client, 'clients/<int:client_id>')
+    def put(self, client_id):
+        abort_if_id_exists(client_id)
+        args = client_args.parse_args()
+        clients[client_id] = args
+        return {'message': 'Client was added...'}
+
+    def delete(self, client_id):
+        abort_if_id_not_exists(client_id)
+        del clients[client_id]
+        return {'message': 'Client was deleted...'}
+
+api.add_resource(Client, '/clients/<int:client_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,34 @@
 import requests
+import time
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE + 'transactions/all')
+print('\n \n \n \n')
+
+response = requests.put(BASE + 'clients/2000', {'full_name': 'James Watson', 'phone_number': '555888999', 'email_address': 'watson@gmail.com'})
+print(response.json())
+
+time.sleep(3)
+
+response = requests.get(BASE + 'clients/2000')
+print(response.json())
+
+time.sleep(3)
+
+response = requests.put(BASE + 'clients/2000', {'full_name': 'James Watson', 'phone_number': '555888999', 'email_address': 'watson@gmail.com'})
+print(response.json())
+
+time.sleep(3)
+
+response = requests.delete(BASE + 'clients/2001')
+print(response.json())
+
+time.sleep(3)
+
+response = requests.delete(BASE + 'clients/2000')
+print(response.json())
+
+time.sleep(3)
+
+response = requests.get(BASE + 'clients/2000')
 print(response.json())
